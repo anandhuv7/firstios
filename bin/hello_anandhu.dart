8f1f310f29e0fe9cf47dbfe3b9c2728c96c73af3
anandhuv7 <138656071+anandhuv7@users.noreply.github.com>
2023-09-25 11:40:42 +0530

    Add files via upload

---
+++ bin/hello_anandhu.dart
@@ -0,0 +1,5 @@
+import 'package:hello_anandhu/hello_anandhu.dart' as hello_anandhu;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hello_anandhu.calculate()}!');
+}
---
+++ test/hello_anandhu_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_anandhu/hello_anandhu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
